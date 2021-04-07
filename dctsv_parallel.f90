848ec919296f999b99e767faf0634e8b32a1e0d8
knoppix <knoppix@Microknoppix>
2021-04-07 14:33:03 -0400

    nothing

--- dctsv_parallel.f90
+++ dctsv_parallel.f90
@@ -62,6 +62,7 @@
 !      ..
        REAL(wp) :: DM(N+mod(N,2)), DLM(N+mod(N,2)), DUM(N+mod(N,2))  ! padded versions
        REAL(wp) :: BM( N+mod(N,2), NRHS )
+
        REAL(wp) :: ud(2,2,0:N/2+1),ue(2,0:N/2+1,NRHS),DET,X(NRHS),Y(NRHS)  !  ud is my set of matrices Aj ue is my vectors vj 
        INTEGER :: i,j,p  
 
@@ -136,7 +137,31 @@
          INFO=j
          CALL XERBLA( 'DCTSV ', INFO )
          RETURN
-        ENDIF                                                                                              
+        ENDIF  
+
+        DET=D(j)*D(1-j+N)+DL(j)*D(1-j+N)*ud(1,1,-1+j)-& 
+            DL(j)*DU(1-j+N)*ud(1,2,-1+j)*ud(2,1,-1+j)+&       
+            D(j)*DU(1-j+N)*ud(2,2,-1+j)+&
+            DL(j)*DU(1-j+N)*ud(1,1,-1+j)*ud(2,2,-1+j)
+
+
+        IF (DET /= 0) THEN
+         ud(1,1,j)=-((DU(j)*(D(1-j+N)+DU(1-j+N)*ud(2,2,-1+j)))/DET)    
+         ud(1,2,j)=(DL(j)*DL(1-j+N)*ud(1,2,-1+j))/DET                 
+         ud(2,1,j)=(DU(j)*DU(1-j+N)*ud(2,1,-1+j))/DET                
+         ud(2,2,j)=-((DL(1-j+N)*(D(j)+DL(j)*ud(1,1,-1+j)))/DET)
+         ue(1,j,1:NRHS)=(-DL(j)*(B(1-j+N,1:NRHS)-DU(1-j+N)*ue(2,-1+j,1:NRHS))*ud(1,2,-1+j)+&             
+                (B(j,1:NRHS)-DL(j)*ue(1,-1+j,1:NRHS))*(D(1-j+N)+DU(1-j+N)*ud(2,2,-1+j)))/DET
+         ue(2,j,1:NRHS)=((B(1-j+N,1:NRHS)-DU(1-j+N)*ue(2,-1+j,1:NRHS))*(D(j)+DL(j)*ud(1,1,-1+j))-&
+                DU(1-j+N)*(B(j,1:NRHS)-DL(j)*ue(1,-1+j,1:NRHS))*ud(2,1,-1+j))/DET    
+
+      
+        ELSE
+         INFO=j
+         CALL XERBLA( 'DCTSV ', INFO )
+         RETURN
+        ENDIF
+                                                                                            
        end do 
 
 !      LAST EQUATION 
